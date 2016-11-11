a41874a612f9c0bbc55869967a725f5a39a73922
= <margaryan.mher.28@gmail.com>
2016-11-11 17:43:33 +0400

    Front end completed.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
-  def new
+  def create
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.create(comments_params)
+
+  	flash[:notice] = 'Comment added.'
+  	redirect_to post_path(@post)
   end
+
+  private
+  	def comments_params
+  		params.require(:comment).permit(:name, :email, :body, :post_id)
+  	end
 end
